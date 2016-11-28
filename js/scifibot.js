36b713ab785824fdf20228903793cbba185473d6
Fernando Bevilacqua <dovyski@gmail.com>
2016-11-28 22:55:19 +0100

    Initial commit (nothing is working, but it looks cool already :D)

---
+++ js/scifibot.js
@@ -0,0 +1,78 @@
+// Initialize your app
+var myApp = new Framework7({
+    init: false //Disable App's automatica initialization
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
+
+// Callbacks to run specific code for specific pages
+myApp.onPageInit('index', function (page) {
+    console.log('ddd');
+    // run createContentPage func after link was clicked
+    $$('.open-movie').on('click', function () {
+        //createContentPage();
+
+        //- With callbacks on click
+        var buttons = [
+            {
+                text: 'Button1',
+                onClick: function () {
+                    myApp.alert('Button1 clicked');
+                }
+            },
+            {
+                text: 'Button2',
+                onClick: function () {
+                    myApp.alert('Button2 clicked');
+                }
+            },
+            {
+                text: 'Cancel',
+                color: 'red',
+                onClick: function () {
+                    myApp.alert('Cancel clicked');
+                }
+            },
+        ];
+        myApp.actions(buttons);
+    });
+});
+
+//And now we initialize app
+myApp.init();
