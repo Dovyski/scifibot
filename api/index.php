a745977f8fbeecbe9b7cbe1aaffb3796c9694424
Fernando Bevilacqua <dovyski@gmail.com>
2016-12-26 19:38:26 +0100

    Add API methods "titles" and "sync"

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-@require_once(dirname(__FILE__) . '/config.local.php');
-require_once(dirname(__FILE__) . '/config.php');
+@include(dirname(__FILE__) . '/config.local.php');
+include(dirname(__FILE__) . '/config.php');
 
 $aMethod = isset($_REQUEST['method']) ? $_REQUEST['method'] : '';
 $aReturn = array('success' => true);
@@ -10,7 +10,7 @@ $aDb = new PDO('sqlite:' . DB_FILE);
 $aDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 switch ($aMethod) {
-	case 'list':
+	case 'titles':
 		$aStmt = $aDb->prepare("SELECT * FROM titles WHERE 1");
 		$aStmt->execute();
 
@@ -21,6 +21,28 @@ switch ($aMethod) {
 		}
 
 		$aReturn['data'] = $aRows;
+		break;
+
+	case 'sync':
+		$aSince = isset($_REQUEST['since']) ? $_REQUEST['since'] : '';
+
+		if($aSince == '') {
+			$aReturn = array('failure' => true, 'message' => 'Missing or invalid "since" param.');
+
+		} else {
+			$aStmt = $aDb->prepare("SELECT * FROM titles WHERE modified >= :since");
+
+			$aStmt->bindParam(':since', $aSince);
+			$aStmt->execute();
+
+			$aRows = array();
+
+			while($aRow = $aStmt->fetch(PDO::FETCH_OBJ)) {
+				$aRows[] = $aRow;
+			}
+
+			$aReturn['data'] = $aRows;
+		}
 		break;
 
 	default:
